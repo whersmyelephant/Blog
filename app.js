4032b0b3ba704b6588b18230406f434003f4342f
evandesamore <evan.desamore@ynov.com>
2025-06-15 22:55:19 +0200

    Premier commit depuis VS Code

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+//Templating engine
+app.use(expressLayouts);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
+
+app.use('/', require('./server/routes/main'));
+
+app.listen(PORT, ()=> {
+    console.log(`App listening on port ${PORT}`);
+})
