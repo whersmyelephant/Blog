3df374afb3441ecf301c55bc2aca714967708f5a
evandesamore <evan.desamore@ynov.com>
2025-06-23 22:44:05 +0200

    Ajout de la connexion à la base de données MongoDB sur le projet NodeJs-Blog du cluster Lug

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
+const connectDB = require('./server/config/db');
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+
+//Connect Database
+
+connectDB();
 app.use(express.static('public'));
 
-//Templating engine
+//Templating Engine 
+
 app.use(expressLayouts);
 app.set('layout','./layouts/main');
 app.set('view engine','ejs');
