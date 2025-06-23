7e02c1f3bf66a38f858490f71e62c7bb9c42f60d
evandesamore <evan.desamore@ynov.com>
2025-06-24 01:16:03 +0200

    Problème Post.count is not a function

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,20 +2,57 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+
 // Routes
 router.get('', async (req, res) => {
-    const locals = {
-        title: "Evan's Blog",
-        description: "Blog project created with NodeJS and MongoDB."
-    }
+
 
     try {
-        const data = await Post.find();
-        res.render('index', { locals,data });
+        const locals = {
+            title: "Evan's Blog",
+            description: "Blog project created with NodeJS and MongoDB."
+        }
+//Nombre de blocs à disposer par page
+            let perPage = 6;
+            let page = req.query.page || 1;
+           
+            //Afficher les posts en les triants du plus ancien en premier grâce à un "$sort: { createdAt: -1 }"
+            const data = await Post.aggregate([{ $sort: { createdAt: -1 } } ])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+            const count = await Post.count();
+            const nextPage = parseInt(page) + 1;
+            const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+            res.render('index',{
+                locals,
+                data,
+                current: page,
+                nextPage: hasNextPage ? nextPage : null
+            });
+
+        
+       // res.render('index', { locals,data });
     } catch (error) {
         console.log(error);
     }
 });
+
+//router.get('', async (req, res) => {
+//    const locals = {
+//        title: "Evan's Blog",
+//        description: "Blog project created with NodeJS and MongoDB."
+//    }
+//
+//    try {
+//        const data = await Post.find();
+//        res.render('index', { locals,data });
+//    } catch (error) {
+//        console.log(error);
+//    }
+//});
 
 router.get('/about',(req, res) => {
     res.render('about');
