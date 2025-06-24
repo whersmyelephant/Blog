26acf35d8c4e3380eac06afe764b9a2a4fa4149b
evandesamore <evan.desamore@ynov.com>
2025-06-24 13:23:31 +0200

    test failure on landing pages

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/register', (req, res) => res.render('register'));
-router.post('/register', userController.register);
-router.get('/login', (req, res) => res.render('login'));
-router.post('/login', userController.login);
-router.get('/logout', userController.logout);
+router.get('/users/register', (req, res) => res.render('register'));
+router.post('/users/register', userController.register);
+router.get('/users/login', (req, res) => res.render('login'));
+router.post('/users/login', userController.login);
+router.get('/users/logout', userController.logout);
 
 module.exports = router;
